f69d6dad52df7c1f0a7a87d735669ed24499d206
Anna Valeria Makari <anmakaro@c2r10s5.42prague.com>
2026-06-20 14:15:30 +0200

    ackermann

--- 01_Recursion and higher-order functions/ex02/ackermann.ml
+++ 01_Recursion and higher-order functions/ex02/ackermann.ml
@@ -5,8 +5,14 @@ let rec ackermann m n =
   else ackermann (m - 1) (ackermann m (n - 1))
 
 let () =
+  Printf.printf "%d\n" (ackermann 2 (-2));
   Printf.printf "%d\n" (ackermann (-1) 7);
   Printf.printf "%d\n" (ackermann 0 0);
+  Printf.printf "%d\n" (ackermann 1 1);
+  Printf.printf "%d\n" (ackermann 2 2);
+  Printf.printf "%d\n" (ackermann 3 3);
   Printf.printf "%d\n" (ackermann 2 3);
   Printf.printf "%d\n" (ackermann 3 4);
-  Printf.printf "%d\n" (ackermann 4 1)
+  Printf.printf "%d\n" (ackermann 4 1);
+  Printf.printf "%d\n" (ackermann 0 2);
+  Printf.printf "%d\n" (ackermann 1 0)
