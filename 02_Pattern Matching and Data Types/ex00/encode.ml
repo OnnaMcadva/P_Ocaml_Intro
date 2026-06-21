f5ec04b04050a363e8b201ff59967747110beb27
Anna Valeria Makari <anmakaro@c3r1s3.42prague.com>
2026-06-21 14:17:21 +0200

    sim

--- 02_Pattern Matching and Data Types/ex00/encode.ml
+++ 02_Pattern Matching and Data Types/ex00/encode.ml
@@ -17,20 +17,11 @@ let encode lst =
 
 let () =
   let result = encode ['a'; 'a'; 'a'; 'b'; 'b'; 'c'] in
-  let rec print_pairs = function
-    | [] -> print_char '\n'
-    | (n, c) :: t -> Printf.printf "(%d, %c) " n c; print_pairs t
-  in
-  print_pairs result;
+  List.iter (fun (n, c) -> Printf.printf "(%d, %c) " n c) result;
+  print_char '\n';
   let result2 = encode [1; 1; 2; 3; 3; 3] in
-  let rec print_int_pairs = function
-    | [] -> print_char '\n'
-    | (n, x) :: t -> Printf.printf "(%d, %d) " n x; print_int_pairs t
-  in
-  print_int_pairs result2;
+  List.iter (fun (n, x) -> Printf.printf "(%d, %d) " n x) result2;
+  print_char '\n';
   let result3 = encode ([] : int list) in
-  let rec my_length = function
-    | [] -> 0
-    | _ :: t -> 1 + my_length t
-  in
-  Printf.printf "empty: %d elements\n" (my_length result3)
+  Printf.printf "empty: %d elements\n" (List.length result3)
+
