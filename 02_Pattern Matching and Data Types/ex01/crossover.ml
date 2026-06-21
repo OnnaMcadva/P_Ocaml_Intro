b5f2f551bda025cbd8035b514c57e65579d0b065
OnnaMcadva <anna.anutka@gmail.com>
2026-06-21 10:27:23 +0200

    -h

--- 02_Pattern Matching and Data Types/ex01/crossover.ml
+++ 02_Pattern Matching and Data Types/ex01/crossover.ml
@@ -4,9 +4,14 @@ let crossover lst1 lst2 =
     | [] -> false
     | h :: t -> h = x || mem x t
   in
+  let rec my_rev lst acc =
+    match lst with
+    | [] -> acc
+    | h :: t -> my_rev t (h :: acc)
+  in
   let rec aux lst1 acc =
     match lst1 with
-    | [] -> List.rev acc
+    | [] -> my_rev acc []
     | h :: t ->
       if mem h lst2 then aux t (h :: acc)
       else aux t acc
@@ -15,10 +20,17 @@ let crossover lst1 lst2 =
 
 let () =
   let r1 = crossover [1; 2; 3; 4] [2; 4; 6] in
-  List.iter (fun x -> Printf.printf "%d " x) r1;
-  print_char '\n';
   let r2 = crossover ['a'; 'b'; 'c'] ['b'; 'c'; 'd'] in
-  List.iter (fun x -> Printf.printf "%c " x) r2;
-  print_char '\n';
   let r3 = crossover [] [1; 2; 3] in
+  let rec print_ints = function
+    | [] -> print_char '\n'
+    | h :: t -> Printf.printf "%d " h; print_ints t
+  in
+  let rec print_chars = function
+    | [] -> print_char '\n'
+    | h :: t -> Printf.printf "%c " h; print_chars t
+  in
+  print_ints r1;
+  print_chars r2;
   Printf.printf "empty: %d elements\n" (List.length r3)
+  
