0e0bdb3e4aac86b99595a24b7a6ca0a7e8b1e289
Anna Valeria Makari <anmakaro@c3r1s3.42prague.com>
2026-06-21 18:00:23 +0200

    -h

--- 02_Pattern Matching and Data Types/ex00/encode.ml
+++ 02_Pattern Matching and Data Types/ex00/encode.ml
@@ -16,12 +16,17 @@ let encode lst =
   | h :: t -> aux t h 1 []
 
 let () =
-  let result = encode ['a'; 'a'; 'a'; 'b'; 'b'; 'c'] in
-  List.iter (fun (n, c) -> Printf.printf "(%d, %c) " n c) result;
+  let result0 = encode ["ku"; "ku"; "ku"; "ka"; "ka"; "ku"] in
+  List.iter (fun (n, c) -> Printf.printf "(%d, %s) " n c) result0;
+  print_char '\n';
+  let result1 = encode ['a'; 'a'; 'a'; 'b'; 'b'; 'c'] in
+  List.iter (fun (n, c) -> Printf.printf "(%d, %c) " n c) result1;
   print_char '\n';
   let result2 = encode [1; 1; 2; 3; 3; 3] in
   List.iter (fun (n, x) -> Printf.printf "(%d, %d) " n x) result2;
   print_char '\n';
   let result3 = encode ([] : int list) in
-  Printf.printf "empty: %d elements\n" (List.length result3)
-
+  Printf.printf "empty: %d elements\n" (List.length result3);
+  let result4 = encode [(3, 2); (3, 2); (4, 3)] in
+  List.iter (fun (n, (a, b)) -> Printf.printf "(%d, (%d, %d)) " n a b) result4;
+  print_char '\n';
