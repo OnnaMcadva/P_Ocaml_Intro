496c361ef292bae6f15e5f9dc49c276407fcb093
OnnaMcadva <anna.anutka@gmail.com>
2026-06-21 10:34:16 +0200

    -h

--- 02_Pattern Matching and Data Types/ex02/gray.ml
+++ 02_Pattern Matching and Data Types/ex02/gray.ml
@@ -1,11 +1,21 @@
 let gray n =
+  let rec my_rev lst acc =
+    match lst with
+    | [] -> acc
+    | h :: t -> my_rev t (h :: acc)
+  in
+  let rec append l1 l2 =
+    match l1 with
+    | [] -> l2
+    | h :: t -> h :: append t l2
+  in
   let rec gen_gray n =
     if n = 0 then [""]
     else
       let prev = gen_gray (n - 1) in
       let left = List.map (fun s -> "0" ^ s) prev in
-      let right = List.map (fun s -> "1" ^ s) (List.rev prev) in
-      left @ right
+      let right = List.map (fun s -> "1" ^ s) (my_rev prev []) in
+      append left right
   in
   let codes = gen_gray n in
   let rec print_codes = function
@@ -20,3 +30,4 @@ let () =
   gray 1;
   gray 2;
   gray 3
+  
