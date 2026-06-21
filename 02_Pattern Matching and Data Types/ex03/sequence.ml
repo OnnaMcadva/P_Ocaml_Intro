790102805e57600f0fcd0abe6853afb45e68d284
OnnaMcadva <anna.anutka@gmail.com>
2026-06-21 10:38:15 +0200

    -h

--- 02_Pattern Matching and Data Types/ex03/sequence.ml
+++ 02_Pattern Matching and Data Types/ex03/sequence.ml
@@ -1,15 +1,24 @@
 let sequence n =
+  let rec int_to_str n acc =
+    if n = 0 then acc
+    else int_to_str (n / 10) (String.make 1 (Char.chr (48 + n mod 10)) ^ acc)
+  in
   let encode_str s =
-    let len = String.length s in
+    let rec len s i =
+      if String.get s i = '\000' then i
+      else len s (i + 1)
+    in
+    ignore len;
+    let slen = String.length s in
     let rec aux i cur count acc =
-      if i >= len then
-        acc ^ string_of_int count ^ String.make 1 cur
+      if i >= slen then
+        acc ^ int_to_str count "" ^ String.make 1 cur
       else
         let c = String.get s i in
         if c = cur then aux (i + 1) cur (count + 1) acc
-        else aux (i + 1) c 1 (acc ^ string_of_int count ^ String.make 1 cur)
+        else aux (i + 1) c 1 (acc ^ int_to_str count "" ^ String.make 1 cur)
     in
-    if len = 0 then ""
+    if slen = 0 then ""
     else aux 1 (String.get s 0) 1 ""
   in
   let rec aux i s =
