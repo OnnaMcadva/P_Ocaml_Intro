bff69ac3e22b7f12bd2a6b8c0ca9bace08652522
Anna Valeria Makari <anmakaro@c3r1s3.42prague.com>
2026-06-21 16:23:14 +0200

    -h

--- 02_Pattern Matching and Data Types/ex02/gray.ml
+++ 02_Pattern Matching and Data Types/ex02/gray.ml
@@ -1,11 +1,26 @@
 let gray n =
+  let rec my_rev lst acc =
+    match lst with
+    | [] -> acc
+    | h :: t -> my_rev t (h :: acc)
+  in
+  let rec append l1 l2 =
+    match l1 with
+    | [] -> l2
+    | h :: t -> h :: append t l2
+  in
+  let rec my_map f lst =
+    match lst with
+    | [] -> []
+    | h :: t -> f h :: my_map f t
+  in
   let rec gen_gray n =
     if n = 0 then [""]
     else
       let prev = gen_gray (n - 1) in
-      let left = List.map (fun s -> "0" ^ s) prev in
-      let right = List.map (fun s -> "1" ^ s) (List.rev prev) in
-      left @ right
+      let left = my_map (fun s -> "0" ^ s) prev in
+      let right = my_map (fun s -> "1" ^ s) (my_rev prev []) in
+      append left right
   in
   let codes = gen_gray n in
   let rec print_codes = function
