3db325fcec306a6d6f3ef0aca722aa71b66641af
OnnaMcadva <anna.anutka@gmail.com>
2026-07-08 20:31:56 +0200

    ex00

--- 03_OCaml’s modules language/ex00/main.ml
+++ 03_OCaml’s modules language/ex00/main.ml
@@ -6,9 +6,9 @@ let rec print_all = function
 			print_all rest
 
 let () =
-	print_endline "\nTesting Color.all:";
+	print_endline "\n\x1b[4;32mTesting Color.all:\x1b[0m";
 	print_all Color.all;
-	print_endline "\nTesting direct calls:";
+	print_endline "\n\x1b[4;32mTesting direct calls:\x1b[0m";
 	print_endline (Color.toString Color.Spade);
 	print_endline (Color.toString Color.Heart);
 	print_endline (Color.toStringVerbose Color.Diamond);
