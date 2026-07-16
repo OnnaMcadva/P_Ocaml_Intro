f66007925947533fefc5b03ea099fcc45f3bbbce
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2026-07-16 09:34:09 +0200

    Update main.ml

--- 06_Object Oriented Programming/ex01/main.ml
+++ 06_Object Oriented Programming/ex01/main.ml
@@ -1,11 +1,17 @@
 let () =
-  let sidekick = new people "Clara" in
-  let doc = new doctor "The Doctor" 900 sidekick in
-  print_endline (doc#to_string);
-  doc#talk;
-  doc#travel_in_time 2015 2020;
-  print_endline (doc#to_string);
-  doc#use_sonic_screwdriver;
-  doc#heal;
-  print_endline (doc#to_string)
+  print_endline "--- Testing Exercise 01: Doctor ---";
+  let rose = new People.people "Rose Tyler" in
+  let doc = new Doctor.doctor "The 10th Doctor" 900 rose in
   
+  print_endline doc#to_string;
+  doc#talk;
+  doc#use_sonic_screwdriver;
+  
+  print_endline "Traveling from 2005 to 2015...";
+  doc#travel_in_time 2005 2015;
+  print_endline doc#to_string;
+  
+  print_endline "Testing regeneration (taking 120 damage)...";
+  doc#take_damage 120;
+  print_endline doc#to_string;
+  print_endline "--- End of Test ---"
