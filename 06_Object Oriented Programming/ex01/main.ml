8bdf8cffe76a3b2e7121b2574387f3f5950caf72
Anna Valeria Makari <anmakaro@c2r10s3.42prague.com>
2026-07-16 10:18:22 +0200

    main

--- 06_Object Oriented Programming/ex01/main.ml
+++ 06_Object Oriented Programming/ex01/main.ml
@@ -10,8 +10,19 @@ let () =
   print_endline "Traveling from 2005 to 2015...";
   doc#travel_in_time 2005 2015;
   print_endline doc#to_string;
+
+  print_endline "Traveling back from 2015 to 2005...";
+  doc#travel_in_time 2015 2005;
+  print_endline doc#to_string;
   
+  print_endline "Testing non-lethal damage (taking 30 damage)...";
+  print_endline ("HP before: " ^ (string_of_int doc#get_hp));
+  doc#take_damage 30;
+  print_endline ("HP after: " ^ (string_of_int doc#get_hp));
+
   print_endline "Testing regeneration (taking 120 damage)...";
+  print_endline ("HP before: " ^ (string_of_int doc#get_hp));
   doc#take_damage 120;
+  print_endline ("HP after: " ^ (string_of_int doc#get_hp));
   print_endline doc#to_string;
   print_endline "--- End of Test ---"
--- 06_Object Oriented Programming/ex03/main.ml
+++ 06_Object Oriented Programming/ex03/main.ml
@@ -8,6 +8,8 @@ let () =
   human_army#add h1;
   human_army#add h2;
   print_endline ("Army size: " ^ (string_of_int (List.length human_army#get_members)));
+  human_army#delete;
+  print_endline ("Human Army size after 1 deletion: " ^ (string_of_int (List.length human_army#get_members)));
 
   print_endline "\n--- 2. Creating Dalek Army ---";
   let dalek_army = new Army.army in
@@ -22,5 +24,19 @@ let () =
   print_endline ("Dalek Army size after 1 deletion: " ^ (string_of_int (List.length dalek_army#get_members)));
   dalek_army#delete;
   print_endline ("Dalek Army size after 2 deletions: " ^ (string_of_int (List.length dalek_army#get_members)));
-  
+
+  print_endline "\n--- 4. Creating Doctor Army ---";
+  let doctor_army = new Army.army in
+  let sidekick1 = new People.people "Amy Pond" in
+  let sidekick2 = new People.people "Clara Oswald" in
+  let doc1 = new Doctor.doctor "The 11th Doctor" 907 sidekick1 in
+  let doc2 = new Doctor.doctor "The 12th Doctor" 2000 sidekick2 in
+  doctor_army#add doc1;
+  doctor_army#add doc2;
+  print_endline ("Doctor Army size: " ^ (string_of_int (List.length doctor_army#get_members)));
+  doctor_army#delete;
+  print_endline ("Doctor Army size after 1 deletion: " ^ (string_of_int (List.length doctor_army#get_members)));
+  doctor_army#delete;
+  print_endline ("Doctor Army size after 2 deletions: " ^ (string_of_int (List.length doctor_army#get_members)));
+
   print_endline "--- End of Test ---"
