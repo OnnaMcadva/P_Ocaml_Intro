b2da4b7d397a14b7712eabf7cddbc806cf717346
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2026-07-16 09:35:35 +0200

    Update dalek.ml

--- 06_Object Oriented Programming/ex02/dalek.ml
+++ 06_Object Oriented Programming/ex02/dalek.ml
@@ -1,47 +1,36 @@
-let random_string len =
-  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" in
-  let rec aux acc n =
-    if n = 0 then acc
-    else
-      let c = String.get chars (Random.int (String.length chars)) in
-      aux (acc ^ String.make 1 c) (n - 1)
-  in
-  aux "" len
-
-class dalek () =
+class dalek =
   object (self)
-    val mutable hp = 100
-    val mutable shield = true
-    val name = "Dalek" ^ (random_string 3)
+    val name : string = 
+      let () = Random.self_init () in
+      let char_pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" in
+      let rand_char () = String.make 1 (String.get char_pool (Random.int (String.length char_pool))) in
+      "Dalek" ^ (rand_char ()) ^ (rand_char ()) ^ (rand_char ())
+    
+    val hp : int = 100
+    val mutable shield : bool = true
 
     method get_name = name
     method get_hp = hp
-    method set_hp new_hp = hp <- new_hp
     method get_shield = shield
-    method toggle_shield = shield <- not shield
 
     method to_string =
-      "Dalek: " ^ name ^ ", HP: " ^ string_of_int hp ^ ", Shield: " ^ string_of_bool shield
+      "Dalek [Name: " ^ name ^ ", HP: " ^ (string_of_int hp) ^ 
+      ", Shield: " ^ (string_of_bool shield) ^ "]"
 
     method talk =
-      let phrases = [
+      let phrases = [|
         "Explain! Explain!";
         "Exterminate! Exterminate!";
         "I obey!";
         "You are the Doctor! You are the enemy of the Daleks!"
-      ] in
-      let idx = Random.int (List.length phrases) in
-      print_endline (List.nth phrases idx)
+      |] in
+      print_endline (Array.get phrases (Random.int 4))
 
-    method exterminate (p : people) =
-      p#set_hp 0;
-      shield <- not shield;
-      print_endline ("Exterminated " ^ p#get_name)
+    method exterminate (target : People.people) =
+      print_endline (name ^ " targets " ^ target#get_name ^ "!");
+      target#die;
+      shield <- not shield
 
     method die =
       print_endline "Emergency Temporal Shift!"
-
-    initializer
-      print_endline ("A new Dalek named " ^ name ^ " has been created!")
   end
-  
